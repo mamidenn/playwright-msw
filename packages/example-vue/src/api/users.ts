cbf245c06c6f0fb017ad973fd4a847838ea14ee5
Martin Dennhardt <martin@dennhardt.de>
2023-11-30 08:36:01 +0000

    preview

--- packages/example-vue/src/api/users.ts
+++
@@ -1,8 +0,0 @@
-import { client } from './client';
-
-export type User = { name: string };
-
-export const getUsers = async () => {
-  const res = await client.get<User[]>('/users');
-  return res.data;
-};
--- packages/example/src/types/search.ts
+++
@@ -1,8 +0,0 @@
-export type SearchResult<TCategory = 'songs' | 'books' | 'movies'> = {
-  title: string;
-  href: string;
-  category: TCategory;
-};
-
-export type GetSearchParams = Record<string, string>;
-export type GetSearchResponse = SearchResult[];
